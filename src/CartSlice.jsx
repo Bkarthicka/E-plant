94849583da5d64da5c28fff1128a24c0d8f57942
Ayush <ayush.tathe@outlook.com>
2024-11-01 00:44:13 +0530

    Added cartitems component functionality

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,14 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      const { name, image, cost } = action.payload;
+      const { name, image, cost } = action.payload; // Rename cost to price
+      
       const existingItem = state.items.find(item => item.name === name);
       if (existingItem) {
         existingItem.quantity++;
       } else {
-        state.items.push({ name, image, cost, quantity: 1 });
+        console.log(cost)
+        state.items.push({ name, image, cost, quantity: 1 }); // Ensure you're adding price
       }
     },
     removeItem: (state, action) => {
@@ -20,11 +22,10 @@ export const CartSlice = createSlice({
     },
     updateQuantity: (state, action) => {
       const { name, quantity } = action.payload;
-const itemToUpdate = state.items.find(item => item.name === name);
-if (itemToUpdate) {
-  itemToUpdate.quantity = quantity;
-}
-    
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
